89eba3c91a5ebd38ff778c2c04195779fee864a6
= <nguyencongquoc3012@gmail.com>
2019-03-03 20:53:43 +0700

    add new file tinh ton.js

---
+++ New folder/tinh ton.js
@@ -0,0 +1,16 @@
+var vatly;
+var sinhhoc;
+var hoahoc
+vatly = prompt(vatly);
+sinhhoc = prompt(sinhhoc);
+hoahoc = prompt(hoahoc);
+vatly = parseInt(vatly);
+sinhhoc = parseInt(sinhhoc);
+hoahoc = parseInt(hoahoc);
+let tong = vatly + sinhhoc + hoahoc;
+let dtb = ((vatly + sinhhoc + hoahoc) / 3);
+document.write("tong=+tong");
+document.write("dtb=+dtb")
+
+
+
